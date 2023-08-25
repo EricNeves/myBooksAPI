bc255a5e8c9c1249edcd7f0dcd16054d08b91378
Eric Neves <ericnevesrodrigues@gmail.com>
2023-08-25 12:33:29 -0400

    Feat: modify cors - index.php

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: *');
+error_reporting(0);
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  header("Access-Control-Allow-Credentials: true");
+  header('Content-type: application/json');
+  http_response_code(200); 
+  exit;
+}
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: OPTIONS, GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
 header('Content-type: application/json');
 
 require_once __DIR__."/config.php";
